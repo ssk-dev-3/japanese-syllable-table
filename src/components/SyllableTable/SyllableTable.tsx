67f9468afa59e8f72d2f5d9d4334465fda755997
ssk-dev-3 <89833989+ssk-dev-3@users.noreply.github.com>
2024-03-20 17:04:37 +0900

    fix audioFilePath

--- src/components/SyllableTable/SyllableTable.tsx
+++ src/components/SyllableTable/SyllableTable.tsx
@@ -1,5 +1,6 @@
 import {Character} from "../Character";
 import styles from "./SyllableTable.module.scss";
+import a from "../../assets/audio/a.m4a";
 
 type Props = {
   isHiragana: boolean;
@@ -8,7 +9,7 @@ type Props = {
 export const SyllableTable: React.FC<Props> = ({isHiragana}) => {
   return (
     <div className={styles.table}>
-      <Character char={isHiragana ? "あ" : "ア"} audioSource="/audio/a.m4a"></Character>
+      <Character char={isHiragana ? "あ" : "ア"} audioSource={a}></Character>
     </div>
   );
 };
