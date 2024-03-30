f27c7b15a1575aee79fe16c66b8f91356a6e1a2c
ssk-dev-3 <89833989+ssk-dev-3@users.noreply.github.com>
2024-03-30 12:49:45 +0900

    PWA対応

--- src/components/KanaTypeSwitch/KanaTypeSwitch.tsx
+++ src/components/KanaTypeSwitch/KanaTypeSwitch.tsx
@@ -11,11 +11,11 @@ export const KanaTypeSwitch: React.FC<Props> = ({isHiragana, onClickRadio}) => {
   return (
     <div className={styles.radioWrapper}>
       <label className={styles.radio}>
-        <input type="radio" name="kanaType" id="hiragana" checked={isHiragana} onClick={() => onClickRadio(true)} onTouchEnd={() => onClickRadio(true)} />
+        <input type="radio" name="kanaType" id="hiragana" checked={isHiragana} onChange={() => onClickRadio(true)} onTouchEnd={() => onClickRadio(true)} />
         <span className={styles.kanaType}>あ</span>
       </label>
       <label className={styles.radio}>
-        <input type="radio" name="kanaType" id="katakana" checked={!isHiragana} onClick={() => onClickRadio(false)} onTouchEnd={() => onClickRadio(false)} />
+        <input type="radio" name="kanaType" id="katakana" checked={!isHiragana} onChange={() => onClickRadio(false)} onTouchEnd={() => onClickRadio(false)} />
         <span className={styles.kanaType}>ア</span>
       </label>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,48 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.GITHUB_PAGES
     ? "japanese-syllable-table"
     : "./",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      injectRegister: 'auto',
+      manifest: {
+        name: 'Japanese Syllable Table',
+        short_name: 'Japanese Syllable Table',
+        description: 'あいうえお表',
+        theme_color: '#ff7d88',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
